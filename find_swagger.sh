5912d733ac7c76846ca7d334c52fd042c238c372
Alejandro Sol <alejandro501@proton.me>
2024-10-17 20:54:35 +0200

    remove chunking

--- find_swagger.sh
+++ find_swagger.sh
@@ -2,7 +2,7 @@
 
 SWAGGER_TEMPLATE="$HOME/nuclei-templates/http/exposures/apis/swagger-api.yaml"
 DOMAINS_FILE="domains.txt"
-MASTER_OUTPUT_FILE="nuclei_swagger_scan.txt"
+OUTPUT_FILE="nuclei_swagger_scan.txt"
 URL_FILE=" temp_swagger_endpoints.txt"
 FINAL_URL_FILE="swagger_endpoints.txt"
 USE_TOR=false
@@ -41,9 +41,6 @@ if [[ "$USE_TOR" == true ]]; then
     echo "Using Tor for Nuclei scans..."
 fi
 
-> "$MASTER_OUTPUT_FILE"  # Clear master output file
-> "$URL_FILE"            # Clear URL file
-
 if [[ -n "$GOBIN" ]]; then
     NUCLEI_CMD="${GOBIN}/nuclei"
 else
@@ -53,17 +50,9 @@ fi
 # Split the domains file into chunks of 5001 lines
 split -l 5001 "$DOMAINS_FILE" domains_chunk_
 
-chunk_number=1  # Initialize chunk counter
-
 # Process each chunk
 for chunk in domains_chunk_*; do
     echo "Processing chunk: $chunk"
-
-    # Output file for current chunk
-    CHUNK_OUTPUT_FILE="${MASTER_OUTPUT_FILE}_${chunk_number}"
-
-    # Clear the chunk output file
-    > "$CHUNK_OUTPUT_FILE"
 
     while IFS= read -r domain; do
         domain="${domain%/}"
@@ -73,18 +62,12 @@ for chunk in domains_chunk_*; do
         fi
 
         echo "Checking $domain"
-        "$NUCLEI_CMD" -list "$chunk" -t "$SWAGGER_TEMPLATE" -o "$CHUNK_OUTPUT_FILE"
+        "$NUCLEI_CMD" -list "$chunk" -t "$SWAGGER_TEMPLATE" -o "$OUTPUT_FILE"
         
     done < "$chunk"
 
     # Append the URLs found in the current chunk to the master URL file
-    grep -oP '(?<=info] ).*(?= \[)' "$CHUNK_OUTPUT_FILE" >> "$URL_FILE"
-
-    # Clean up the chunk output file after processing
-    # rm "$CHUNK_OUTPUT_FILE"
-
-    # Increment the chunk counter
-    ((chunk_number++))
+    grep -oP '(?<=info] ).*(?= \[)' "$OUTPUT_FILE" >> "$URL_FILE"
     
     # Optionally, remove the chunk file after processing
     rm "$chunk"
@@ -95,7 +78,7 @@ rm domains_chunk_*
 
 # Sort and get unique URLs, saving to the final output file
 sort -u "$URL_FILE" -o "$FINAL_URL_FILE"
-rm $URL_FILE
+# rm $URL_FILE
 
 if [[ "$USE_TOR" == true ]]; then
     unset https_proxy http_proxy
