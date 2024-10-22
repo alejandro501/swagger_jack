08cfc4d7e34787f2c565e68856ef5a01e36ac1f6
Alejandro Sol <alejandro501@proton.me>
2024-10-22 23:03:35 +0200

    add cleanup of api wordlist

--- swagger_jacker.sh
+++ swagger_jacker.sh
@@ -2,11 +2,12 @@
 
 INPUT_FILE="swagger_endpoints.txt"
 OUTPUT_FILE="$HOME/hack/resources/wordlists/swagger-jacker-api-wild.json"
+CLEANED_FILE="swagger-jacker-api-wild.txt"
 SJ_CMD=""
 USE_TOR=false
 GOBIN=""
 
-# Function to parse command-line arguments
+# parse command-line arguments
 parse_arguments() {
     while [[ "$#" -gt 0 ]]; do
         case "$1" in
@@ -26,7 +27,7 @@ parse_arguments() {
     done
 }
 
-# Function to set up Tor if required
+# set up Tor if required
 setup_tor() {
     if [[ "$USE_TOR" == true ]]; then
         export https_proxy=socks5://127.0.0.1:9050
@@ -35,7 +36,7 @@ setup_tor() {
     fi
 }
 
-# Function to determine the swagger jacker command path
+# determine the swagger jacker command path
 get_swagger_jacker_command() {
     if [[ -n "$GOBIN" ]]; then
         SJ_CMD="${GOBIN}/sj"
@@ -44,7 +45,7 @@ get_swagger_jacker_command() {
     fi
 }
 
-# Function to check if the input file exists
+# check if the input file exists
 check_input_file() {
     if [[ ! -f $INPUT_FILE ]]; then
         echo "File not found: $INPUT_FILE"
@@ -52,34 +53,40 @@ check_input_file() {
     fi
 }
 
-# Function to initialize the output file
+# initialize the output file
 initialize_output_file() {
     if [[ ! -f $OUTPUT_FILE ]]; then
         echo "[] " > "$OUTPUT_FILE"
     fi
 }
 
-# Function to process each URL
+# clean up the endpoint file
+clean_endpoint_file() {
+    # remove everything after the first curly brace and trailing slashes, then remove duplicates
+    sed 's/{.*//; s/\/$//' "$CLEANED_FILE" | sort -u > tmp_cleaned.txt && mv tmp_cleaned.txt "$CLEANED_FILE"
+}
+
+# process each URL
 process_urls() {
     while IFS= read -r url; do
         echo "Running sj automate for: $url"
         "$SJ_CMD" automate -u "$url" --outfile swagger_jacker.json
 
-        # Post-process the JSON output and append it to the main file
+        # post-process the JSON output and append it to the main file
         post_process swagger_jacker.json "$OUTPUT_FILE"
     done < "$INPUT_FILE"
 }
 
-# Function to process and append results to the output file
+# process and append results to the output file
 post_process() {
     local json_file="$1"
     local output_file="$2"
     
-    # Append the 'results' array from the new JSON file to the output file
+    # append the 'results' array from the new JSON file to the output file
     jq '.results' "$json_file" | jq -s '.[0] + .' "$output_file" - > tmp.json && mv tmp.json "$output_file"
 }
 
-# Function to clean up after processing
+# clean up after processing
 cleanup() {
     if [[ "$USE_TOR" == true ]]; then
         unset https_proxy http_proxy
@@ -88,16 +95,16 @@ cleanup() {
     echo "New endpoints appended to $OUTPUT_FILE"
 }
 
-# Main function to orchestrate the script
+# main function to orchestrate the script
 main() {
     parse_arguments "$@"
     setup_tor
     get_swagger_jacker_command
     check_input_file
     initialize_output_file
+    clean_endpoint_file
     process_urls
     cleanup
 }
 
-# main
 main "$@"
